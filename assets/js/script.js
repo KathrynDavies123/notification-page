0b8e03ad4187b77ff4f2f5968c24f2efc8752eb7
KathrynDavies123 <kathryn.davies1233@gmail.com>
2022-12-05 16:46:00 +0100

    finished

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let notificationcounter = document.querySelector(".notificationcounter");
+let markasreadbutton = document.querySelector(".markasread");
+let notificationcontainers = document.querySelectorAll(".notificationcontainer");
+
+let counter = notificationcontainers.length;
+
+notificationcounter.innerText = counter;
+
+markasreadbutton.addEventListener("click", function (event) {
+    event.preventDefault();
+    notificationcontainers.forEach((item) => {
+        item.classList.remove("unread");
+    })
+    counter = 0;
+    notificationcounter.innerText = counter;
+})
+
+notificationcontainers.forEach((item) => {
+    item.addEventListener("click", function (event) {
+        event.preventDefault();
+        item.classList.remove("unread");
+        counter -= 1;
+        notificationcounter.innerText = counter;
+    })
+})
